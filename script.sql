f67cb8aab131a8e665ac96b76e063f222b2d7cba
hiraldac <saix.isa@gmail.com>
2018-10-05 10:39:00 -0500

    script

--- script.sql
+++ script.sql
@@ -1,2 +1,11 @@
 create database escuela;
 use escuela;
+
+create table materia(
+idMateria varchar(5) primary key,
+nombre varchar(20),
+creditos integer);
+
+create table licenciatura(
+idLicenciatura varchar(5) primary key,
+nombre varchar(20));
