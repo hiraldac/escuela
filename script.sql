22250d42524a31a595d3669648b0e3074c75fabc
hiraldac <saix.isa@gmail.com>
2018-10-05 18:40:18 -0500

    script completo

--- script.sql
+++ script.sql
@@ -9,3 +9,21 @@ creditos integer);
 create table licenciatura(
 idLicenciatura varchar(5) primary key,
 nombre varchar(20));
+
+create table alumno(
+Matricula varchar(10) primary key,
+nombre varchar(20),
+apellidoP varchar(20),
+apellidoM varchar(20),
+idLicenciatura varchar(5),
+foreign key(idLicenciatura) references licenciatura(idLicenciatura)
+on delete restrict on update cascade);
+
+create table calific(
+idCalif varchar(10) primary key,
+Matricula varchar(10),
+idMateria varchar(5),
+foreign key(Matricula) references alumno(Matricula)
+on delete restrict on update cascade,
+foreign key(idMateria) references materia(idMateria)
+on delete restrict on update cascade);
